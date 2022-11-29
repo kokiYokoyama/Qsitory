0389613d91f2ac6a96f7a6eb0888e4d2c7119096
koki <5519103y@st.toho-u.jp>
2022-11-30 01:52:50 +0900

    add mainprogram

---
+++ qsitory_main.ml
@@ -0,0 +1,28 @@
+(*
+**  Qsitory
+**
+*)
+open Syntax
+open Tools   
+module F = Format
+module P = Pprint
+   
+
+let filename = ref ""
+
+             
+let set_filename fname = filename := fname
+  
+let msgUsage = "USAGE: qsitory [-f <file>]\n"
+
+let speclist = [
+    ("-f", Arg.String set_filename, "Input file");
+    ("-lex", Arg.Unit (fun _ -> addDebugOpt "LEXING"), "Set lexing debug mode");
+    ("-parse", Arg.Unit (fun _ -> addDebugOpt "PARSING"), "Set parsing debug mode");    
+]
+
+let () =
+  Arg.parse speclist print_endline msgUsage;
+  match !filename = "" with
+  | true -> Interactive.interpreter ()
+  | false -> Readfile.interpreter !filename
